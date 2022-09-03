99d285068009c5c6719ffaf24aa23bfc4fd9c186
F. Can Mekikoğlu <fcanmekikoglu@gmail.com>
2022-09-03 19:12:34 +0300

    Add files via upload

---
+++ src/products/product.controller.ts
@@ -0,0 +1,48 @@
+import {Controller, Post, Body, Get, Param, Patch, Delete} from '@nestjs/common'
+import { get } from 'http';
+import { ProductService } from './products.service';
+
+@Controller('products')
+export class ProductsController {
+    constructor(private readonly productsService : ProductService){}
+
+    @Post()
+    addProduct(
+        @Body('title') prodTitle: string,
+        @Body('description') prodDesc: string,
+        @Body('price') prodPrice: number  ):any {
+            const generatedId = this.productsService.insertProduct(
+                prodTitle, 
+                prodDesc, 
+                prodPrice)
+            return {id:generatedId }
+    }
+
+    @Get()
+        getAllProducts (){
+            return this.productsService.getProducts()
+    }
+
+    @Get(':id')
+    getProduct(@Param('id') prodId: string){
+        return this.productsService.getSingleProduct(prodId)
+    }
+
+    @Patch(':id')
+    updateProduct(
+        @Param('id') prodId: string, 
+        @Body('title') prodTitle: string,
+        @Body('description') prodDesc: string,
+        @Body('price') prodPrice: number  ){
+            this.productsService.updateProduct(prodId, prodTitle, prodDesc, prodPrice)
+            return null
+        }
+
+    @Delete(':id')
+    deleteProduct(
+        @Param('id') prodId: string,
+    ){
+        this.productsService.deleteProduct(prodId)
+        return null
+    }
+}
